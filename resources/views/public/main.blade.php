56ad5887d044dd1dafb79880c61246c89c1ccb58
hoanghai03112005 <hoanghai03112k5@gmail.com>
2024-09-20 23:16:59 +0700

    chinh sua lan 1

--- resources/views/public/main.blade.php
+++ resources/views/public/main.blade.php
@@ -38,13 +38,37 @@
 <!--===============================================================================================-->
 </head>
 
-<body class="">
-<header class="">
+<body class="animsition">
+<header class="{{ request()->routeIs('public.index') ? '' : 'header-v4' }}" >
     <!-- Header desktop -->
+    <div class="container-menu-desktop" style="{{ request()->routeIs('public.index') ? '' : 'margin-bottom: 30px'}}">
+        <div class="top-bar">
+            <div class="content-topbar flex-sb-m h-full container">
+                <div class="left-top-bar">
+                    Free shipping for standard order over $100
+                </div>
 
+                <div class="right-top-bar flex-w h-full">
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        Help & FAQs
+                    </a>
 
-        <div class="wrap-menu-desktop">
-            <nav class="limiter-menu-desktop container">
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        My Account
+                    </a>
+
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        EN
+                    </a>
+
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        USD
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="wrap-menu-desktop {{ request()->routeIs('public.index') ? '' : 'how-shadow1' }}">
+            <nav class="limiter-menu-desktop container" >
                 
                 <!-- Logo desktop -->		
                 <a href="{{ route('public.index') }}" class="logo">
@@ -78,9 +102,7 @@
 
                 <!-- Icon header -->
                 <div class="wrap-icon-header flex-w flex-r-m">
-                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
-                        <i class="zmdi zmdi-search"></i>
-                    </div>
+                    
 
                     <a href="{{ route('cart.view')  }}" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
                         <i class="zmdi zmdi-shopping-cart"></i>
@@ -113,34 +135,7 @@
     </div>
 
     <!-- Header Mobile -->
-    <div class="wrap-header-mobile">
-        <!-- Logo moblie -->		
-        <div class="logo-mobile">
-            <a href="index.html"><img src="assets/images/icons/logo-01.png" alt="IMG-LOGO"></a>
-        </div>
-
-        <!-- Icon header -->
-        <div class="wrap-icon-header flex-w flex-r-m m-r-15">
-            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
-                <i class="zmdi zmdi-search"></i>
-            </div>
-
-            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="2">
-                <i class="zmdi zmdi-shopping-cart"></i>
-            </div>
-
-            <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti" data-notify="0">
-                <i class="zmdi zmdi-favorite-outline"></i>
-            </a>
-        </div>
-
-        <!-- Button show menu -->
-        <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
-            <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-            </span>
-        </div>
-    </div>
+   
 
 
     <!-- Menu Mobile -->
